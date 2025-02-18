f931ad401c8599f72eee966f26b39183f903f90a
MUHAMMED-SHANIF <muhammed.shanif.2468@gmail.com>
2025-02-18 10:01:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Dummy from './components/Dummy'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const newLocal = <you />
+  return (
+    <>
+    <Dummy/>
+    <h1>shanif</h1>
+    <h2>is</h2>
+    <h3>good</h3>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Dummy.jsx
@@ -0,0 +1,9 @@
+import "./Dummy.css";
+function Dummy () {
+    return (
+        <>
+            <h1 id="yu">Dummy Component</h1>
+        </>
+    )
+}
+export default Dummy;
